5210ca3aecb63f6c8713475afe025d0daa37a703
KotiYadav <koti0120@gmail.com>
2025-04-02 09:48:12 +0530

    Docker

--- install-docker.sh
+++ install-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #check wheather root user or not
-R=\e[31m
-N=\e[0m
+R="\e[31m"
+N="\e[0m"
 
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo 
